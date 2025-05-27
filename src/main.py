a46db56cb1e9c00f04bdfb72513775c51e038d6b
suguru shiroishi <s.shiroishi@branu.jp>
2025-05-27 17:06:58 +0900

    corc変更

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS設定を追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://brain-test-app-a10fa.web.app"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
